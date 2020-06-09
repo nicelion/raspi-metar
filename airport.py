c22c4b8c40634f398a45b5f66221c3efc3d9a375
Ian Thompson <ianthompson@nicelion.com>
2020-06-09 15:48:40 -0400

    added dependency back

--- airport.py
+++ airport.py
@@ -1,4 +1,5 @@
 from avwx import Metar, Station
+import settings
 
 class Airport:
     def __init__(self, ident, index):
@@ -8,14 +9,23 @@ class Airport:
         self.station = Station.from_icao(ident)
 
         self.metar = self.__get_metar()
-
     def __get_metar(self):
         m = Metar(self.idnet)
         m.update()
         return m
     
-    def set_led(self, color):
-        print("Setting LED %s to %s for %s" % (self.index, color, self.station.name))
+    def get_led_color(self):
+        flight_rules = self.metar.data.flight_rules
+
+        if flight_rules == 'VFR':
+            return settings.vfr_color
+        elif flight_rules == 'MVFR':
+            return settings.mvfr_color
+        elif flight_rules == 'IFR':
+            return settings.ifr_color
+        elif flight_rules == 'LIFR':
+            return settings.lifr_color
+
 
     def update_airport(self):
         self.set_led("RED")
