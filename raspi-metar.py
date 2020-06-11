77544336baf40d63181aab3c82ce7b347a82d0ec
Ian Thompson <ianthompson@nicelion.com>
2020-06-10 22:42:44 -0400

    reworked lightning status

--- raspi-metar.py
+++ raspi-metar.py
@@ -1,3 +1,7 @@
+# raspi-metar.py
+#
+# Created by Ian Thompson
+
 import avwx
 from avwx import Metar
 import setup
@@ -6,7 +10,6 @@ import time
 import settings
 
 airports = []
-lightning = []
 
 def update_information():
     print('UPDATING METAR INFORMATION')
@@ -22,11 +25,6 @@ def set_leds():
         led_color = airport.get_led_color()
         station = airport.get_station_info()
         flight_rules = airport.get_flight_rules()
-        lightning_status = airport.is_lightning()
-
-        
-        if lightning_status:
-            lightning.append(airport)
 
         print('Setting %s to %s for %s conditions' % (airport.station.name, str(led_color), flight_rules))
 
@@ -38,24 +36,28 @@ def show_animations():
     limit = settings.refresh_rate / 2
     count = 0
     while count < limit:
-        for airport in lightning:
-            name = airport.get_station_info().name
-            print('setting %s to WHITE' % name)
+        for airport in airports:
+            if airport.is_lightning():
+                name = airport.get_station_info().name
+                print('setting %s to WHITE' % name)
 
         time.sleep(1)
-        for airport in lightning:
-            name = airport.get_station_info().name
-            print('setting %s to %s' % (name, airport.get_led_color()))
+        
+        for airport in airports:
+            if airport.is_lightning():
+                name = airport.get_station_info().name
+                print('setting %s to %s' % (name, airport.get_led_color()))
         time.sleep(1)
 
         count += 1
 
-try:
-    while True:
-        update_information()
+if __name__ == "__main__":
+    try:
+        while True:
+            update_information()
 
-        set_leds()
+            set_leds()
 
-        show_animations()
-except KeyboardInterrupt:
-    print("cleaning up")
+            show_animations()
+    except KeyboardInterrupt:
+        print("cleaning up")
