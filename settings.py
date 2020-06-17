f74c04e07df2e4e4276161a3a8b8a432c76e7852
Ian Thompson <ianthompson@nicelion.com>
2020-06-16 21:59:25 -0400

    added animation duration

--- settings.py
+++ settings.py
@@ -6,6 +6,7 @@ config.sections()
 
 airports = config['airports']
 refresh_rate = config.getint('settings', 'refresh_rate')
+animation_duration = config.getint('settings', 'animation_duration')
 show_lightning = config.getboolean('settings', 'show_lightning')
 brightness = config.getint('settings', 'brightness')
 i2c_display_enabled = config.getboolean('settings', 'i2c_display_enabled')
@@ -14,3 +15,6 @@ vfr_color = config['colors']['vfr']
 ifr_color = config['colors']['ifr']
 mvfr_color = config['colors']['mvfr']
 lifr_color = config['colors']['lifr']
+snow_color = config['colors']['snow']
+tornado_color = config['colors']['tornado']
+fog_color = config['colors']['fog']
