2a2f20518d4f229a2d735bcdd9e162f92e442bae
Ian Thompson <ianthompson@nicelion.com>
2020-06-24 22:53:57 -0400

    add all LEDS

--- setup.py
+++ setup.py
@@ -2,67 +2,11 @@ from os import path, system
 from avwx import Metar, exceptions, Station
 from configparser import ConfigParser
 
-done_keywords = ["done", "finished", "exit", "quit", "save"]
+print("raspi-metar Setup Wizard")
+play = True
 
-def start_wizard():
-    # start the setup wizard
-    system('clear')
-    print('Welcome to Setup Wizard')
-
-def get_command():
-    # get the input from user and decide what to do with it. If command does not exist, function gets called again
-    
-    comm = input("Enter a command to get started: ")
-
-    if comm == 'help':
-        system('cat support/helpfile')
-    elif comm == "add all":
-        add_all()
-    elif comm == 'add at':
-        print('add at')
-    elif comm in done_keywords:
-        print('Goodbye.')
-        return False
-    else: 
-        print('[ERROR]: Invalid command. Please try again. Pass help for all commands')
-    
-    return True
-        
-
-def verify_airport_is_valid() -> (str, str):
-    # verifies ICAO ident provided is a valid ident. If not, user will be asked to provide a valid one before going on.
-
-    while True:
-    
-        a = input("    Enter ICAO ident (Kxxx): ")
-        if a in done_keywords:
-            return ('done', 'done')
-            break
-        try:
-            stat = Station.from_icao(a)
-            return (a.upper(), stat.name)
-            break
-        except exceptions.BadStation:
-            print("[ERR] %s is not a valid identifier!" % a)
-
-def add_all():
-    print('Assign ICAO idents to LED position. For example, when asked you would pas \'KJFK\' and \'10\' if you wanted to assign John F. Kennedy International Airport to use LED 10.\nWhen finished, pass done\n')
-    
-    config = {}
-    while True:
-
-        airport = verify_airport_is_valid()
-
-        if airport[0] == 'done':
-            save_airports(config)
-            break
-        else:
-            led = input("    LED position: ")
-
-            config.update({led: airport[0]}) 
-
-            print('[SUCCESS]: Assigned %s to LED at positon %s' % (airport[1], led))
-            
+led_index = 1
+config = {}
 
 def save_airports(configuration):
     config =  ConfigParser()
@@ -79,10 +23,22 @@ def save_airports(configuration):
     else:
         print("Ok. Not saving recent configuration")
 
+while play:
 
-if __name__ == "__main__":
-    start_wizard()
+    try:
+        ident = input("ICAO identifier for LED #%s: " % led_index)
 
-    loop = True
-    while loop:
-        loop = get_command()
+        try: 
+            m = Metar(ident.upper())
+            config.update({led_index: ident})
+            print('Assigning %s to LED #%s' % (m.station.name, led_index))
+            led_index += 1
+        except:
+            print('[ERROR] %s is not a valid ICAO identifier! Be sure to include county code!' % ident)
+
+
+
+    except KeyboardInterrupt:
+        print("Keyboard")
+        save_airports(config)
+        play = False
